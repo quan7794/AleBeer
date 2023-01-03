c2c66ae35a03b8047dfee4192b4e8f4be531d0f8
Quanmt Home <quanmtfu@gmail.com>
2023-01-04 01:47:15 +0700

    Move to kotlin dir.

--- app/src/main/kotlin/app/wac/team/wacbase/ui/feature/firstScreen/FirstScreenFragment.kt
+++ app/src/main/kotlin/app/wac/team/wacbase/ui/feature/firstScreen/FirstScreenFragment.kt
@@ -1,21 +1,15 @@
-package app.wac.team.wacbase
+package app.wac.team.wacbase.ui.feature.firstScreen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import app.wac.team.wacbase.R
 
 class FirstScreenFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
         return inflater.inflate(R.layout.fragment_first_screen, container, false)
     }
 }
